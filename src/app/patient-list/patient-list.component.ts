a37ecda6bc30baead88595bf5b11d9b754c56722
aissatou <72610380+aissou20@users.noreply.github.com>
2021-12-10 12:46:33 +0100

    chore(Login) :adding authentication with auth0

--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Patient } from '../models/patient';
+import {Component, OnInit} from '@angular/core';
+import {Patient} from '../models/patient';
+import {AuthService} from '@auth0/auth0-angular';
+import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-patient',
   templateUrl: './patient-list.component.html',
@@ -7,16 +10,23 @@ import { Patient } from '../models/patient';
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private auth: AuthService, router: Router) {
   }
 
-  patient :Patient ={
+  patient: Patient = {
     firstname: 'patient1',
-    lastname:'patient-lastname',
-    email:'patientemail@mail.com'
-}
+    lastname: 'patient-lastname',
+    email: 'patientemail@mail.com'
+  }
+
+  ngOnInit(): void {
+    // this.auth.user$.subscribe(user => {
+    //     if (!user) {
+    //       this.router.navigate(['/login'])
+    //     }
+    //   }
+    // )
+  }
 
 
 }
